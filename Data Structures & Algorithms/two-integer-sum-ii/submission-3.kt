44548c7ff74d984a510470c4c3c4fa333d55adf8
Amr Mahmoud <amrmahmouddev05@gmail.com>
2026-07-21 12:45:16 +0300

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var l = 0;
+        var r = nums.lastIndex;
+        var currSum = 0
+        while ( l != r){
+            currSum = nums[l]+nums[r]
+            when {
+                currSum > target  -> r-- 
+                currSum < target  -> l++
+                else -> break 
+            }
+        }
+        return intArrayOf(++l,++r)
+    }
+}
